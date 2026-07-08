6dc13ff9c50467097bbdf2787dc913358fc54be5
hanjala2p <mdhanjala016pc@gmail.com>
2026-07-08 20:57:16 +0600

    recheking

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -21,7 +21,7 @@ const Categories = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {data?.slice(0, 4).map((plant) => (
+          {data?.slice(0, 3).map((plant) => (
             <Card
               key={plant.plantId}
               plant={plant}
